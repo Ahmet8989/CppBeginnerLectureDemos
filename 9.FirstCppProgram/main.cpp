594b547775de9b086d464dbd3dc749c00647bea6
Ahmet Behdioğlu <ahmetuestcostaj@gmail.com>
2025-05-01 23:46:54 +0300

    hello world stage completed

---
+++ 9.FirstCppProgram/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+consteval int get_value(){
+    return 3;
+}
+
+int main(){
+    // One line comment 
+    /*
+        Multi-line
+        Block
+        Comment
+    */
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
